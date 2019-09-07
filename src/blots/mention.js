a9a1871fe396672ab3509921b1f8cb4e4fb8cd88
PsychoBunny1 <54643357+PsychoBunny1@users.noreply.github.com>
2019-09-07 19:15:57 +0400

    Update mention.js

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -1,34 +1,56 @@
-import Quill from 'vue-quill-editor/node_modules/quill';
-
-const Embed = Quill.import('blots/embed');
-
-
-class MentionBlot extends Embed {
-  static create(data) {
-    const node = super.create();
-    const denotationChar = document.createElement('span');
-    denotationChar.className = 'ql-mention-denotation-char';
-    denotationChar.innerHTML = data.denotationChar;
-    node.appendChild(denotationChar);
-    node.innerHTML += data.value;
-    return MentionBlot.setDataValues(node, data);
-  }
-
-  static setDataValues(element, data) {
-    const domNode = element;
-    Object.keys(data).forEach((key) => {
-      domNode.dataset[key] = data[key];
-    });
-    return domNode;
-  }
-
-  static value(domNode) {
-    return domNode.dataset;
-  }
-}
-
+"use strict";
+var _quill = _interopRequireDefault(require("vue-quill-editor/node_modules/quill"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+var Embed = _quill.default.import('blots/embed');
+var MentionBlot =
+    /*#__PURE__*/
+    function (_Embed) {
+        _inherits(MentionBlot, _Embed);
+        function MentionBlot() {
+            _classCallCheck(this, MentionBlot);
+            return _possibleConstructorReturn(this, _getPrototypeOf(MentionBlot).apply(this, arguments));
+        }
+        _createClass(MentionBlot, null, [{
+            key: "create",
+            value: function create(data) {
+                var node = _get(_getPrototypeOf(MentionBlot), "create", this).call(this);
+                var denotationChar = document.createElement('span');
+                denotationChar.className = 'ql-mention-denotation-char';
+                denotationChar.innerHTML = data.denotationChar;
+                node.appendChild(denotationChar);
+                node.innerHTML += data.value;
+                return MentionBlot.setDataValues(node, data);
+            }
+        }, {
+            key: "setDataValues",
+            value: function setDataValues(element, data) {
+                var domNode = element;
+                Object.keys(data).forEach(function (key) {
+                    domNode.dataset[key] = data[key];
+                });
+                return domNode;
+            }
+        }, {
+            key: "value",
+            value: function value(domNode) {
+                return domNode.dataset;
+            }
+        }]);
+        return MentionBlot;
+    }(Embed);
 MentionBlot.blotName = 'mention';
 MentionBlot.tagName = 'span';
 MentionBlot.className = 'mention';
-
-Quill.register(MentionBlot);
+_quill.default.register(MentionBlot);
